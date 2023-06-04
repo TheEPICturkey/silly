6f1a8f71fe2476aef099fcfcd3c3deb66f9c25a9
Brandon Spear <Brandon.Spear97@gmail.com>
2023-06-04 12:03:47 -0700

    add mysql file

---
+++ SillystringzsFactory/Factory/Models/Machine.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public int MachineId { get; set; }
+
+    [Required(ErrorMessage = "You must enter a machine name.")]
+    public string MachineName { get; set; }
+
+    [Required(ErrorMessage = "Please enter a description of the machine.")]
+    public string MachineDescription { get; set; }
+    public List<EngineerMachine> JoinEntities { get;}
+  }
+}
